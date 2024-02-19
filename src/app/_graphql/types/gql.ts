889d44072140e72d48a3e514e164f026955de2c6
George Ndirangu <ndirangu.mepawa@gmail.com>
2024-02-19 13:33:24 +0300

    feat: implement filter by character and episode name

--- src/app/_graphql/types/gql.ts
+++ src/app/_graphql/types/gql.ts
@@ -13,6 +13,8 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "query LocationsByCharacterQuery($page: Int, $name: String) {\n  characters(page: $page, filter: {name: $name}) {\n    info {\n      pages\n      count\n      next\n      prev\n    }\n    results {\n      image\n      id\n      name\n      status\n      location {\n        id\n        name\n        type\n      }\n    }\n  }\n}": types.LocationsByCharacterQueryDocument,
+    "query LocationsByEpisodeQuery($page: Int, $name: String) {\n  episodes(page: $page, filter: {name: $name}) {\n    info {\n      pages\n      count\n      next\n      prev\n    }\n    results {\n      characters {\n        image\n        id\n        name\n        status\n        location {\n          id\n          name\n          type\n        }\n      }\n    }\n  }\n}": types.LocationsByEpisodeQueryDocument,
     "query LocationsQuery($page: Int, $name: String) {\n  locations(page: $page, filter: {name: $name}) {\n    info {\n      pages\n      count\n      next\n      prev\n    }\n    results {\n      id\n      name\n      type\n      residents {\n        image\n        id\n        name\n        status\n      }\n    }\n  }\n}": types.LocationsQueryDocument,
 };
 
@@ -30,6 +32,14 @@ const documents = {
  */
 export function graphql(source: string): unknown;
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query LocationsByCharacterQuery($page: Int, $name: String) {\n  characters(page: $page, filter: {name: $name}) {\n    info {\n      pages\n      count\n      next\n      prev\n    }\n    results {\n      image\n      id\n      name\n      status\n      location {\n        id\n        name\n        type\n      }\n    }\n  }\n}"): (typeof documents)["query LocationsByCharacterQuery($page: Int, $name: String) {\n  characters(page: $page, filter: {name: $name}) {\n    info {\n      pages\n      count\n      next\n      prev\n    }\n    results {\n      image\n      id\n      name\n      status\n      location {\n        id\n        name\n        type\n      }\n    }\n  }\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query LocationsByEpisodeQuery($page: Int, $name: String) {\n  episodes(page: $page, filter: {name: $name}) {\n    info {\n      pages\n      count\n      next\n      prev\n    }\n    results {\n      characters {\n        image\n        id\n        name\n        status\n        location {\n          id\n          name\n          type\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query LocationsByEpisodeQuery($page: Int, $name: String) {\n  episodes(page: $page, filter: {name: $name}) {\n    info {\n      pages\n      count\n      next\n      prev\n    }\n    results {\n      characters {\n        image\n        id\n        name\n        status\n        location {\n          id\n          name\n          type\n        }\n      }\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- src/app/api/location/route.ts
+++ src/app/api/location/route.ts
@@ -1,10 +1,8 @@
+import { Character, Characters, Episode, Episodes, Location, Locations, LocationsByCharacterQueryDocument, LocationsByCharacterQueryQueryVariables, LocationsByEpisodeQueryDocument, LocationsByEpisodeQueryQueryVariables, LocationsQueryDocument, LocationsQueryQueryVariables } from '@/app/_graphql/types/graphql';
 import { rickAndMortyEndpoint } from '@/config';
-import { Locations, LocationsQueryDocument, LocationsQueryQueryVariables } from '@/app/_graphql/types/graphql';
 import { request as gqlRequest } from 'graphql-request';
 import { NextRequest } from 'next/server';
 
-//TODO REPLACE THIS WITH GRAPHQL, GRAPHQL FETCHES DATA MORE EFFECTIVELY,
-//less network calls
 export async function GET(request: NextRequest) {
     try {
         const searchParams = request.nextUrl.searchParams
@@ -12,39 +10,102 @@ export async function GET(request: NextRequest) {
         const character = searchParams.get('character')
         const episode = searchParams.get('episode')
         const page = searchParams.get('page')
-        let locations;
+        let locations: Locations | undefined;
 
         let pageNumber: number | undefined;
         if (page && Number.isInteger(parseInt(page))) {
             pageNumber = parseInt(page);
-            //todo handle invalid page number
         }
 
-        let characterId: number | undefined;
-        if (character && Number.isInteger(parseInt(character))) {
-            characterId = parseInt(character);
-            //todo fetch from character and page
+        if (character) {
+            const variables: LocationsByCharacterQueryQueryVariables = {
+                page: pageNumber,
+                name: character
+            }
+            const charactersData = await gqlRequest(rickAndMortyEndpoint, LocationsByCharacterQueryDocument, variables)
+            console.log("charctersData", JSON.stringify(charactersData))
+            locations = transformCharactersResponseToLocation(charactersData.characters as Characters)
 
-            return Response.json({ data: "character " + characterId })
+        } else if (episode) {
+            const variables: LocationsByEpisodeQueryQueryVariables = {
+                page: pageNumber,
+                name: episode
+            }
+            const episodesData = await gqlRequest(rickAndMortyEndpoint, LocationsByEpisodeQueryDocument, variables)
+            console.log("episodesData", episodesData)
+            locations = transformEpisodesResponseToLocation(episodesData.episodes as Episodes)
+        } else {
+            const variables: LocationsQueryQueryVariables = {
+                page: pageNumber,
+                name: name
+            }
+            locations = (await gqlRequest(rickAndMortyEndpoint, LocationsQueryDocument, variables)).locations as Locations
         }
 
-        let episodeId: number | undefined;
-        if (episode && Number.isInteger(parseInt(episode))) {
-            episodeId = parseInt(episode);
-            //todo fetch from episideid and page
-            return Response.json({ data: "episode " + episodeId })
-        }
 
-
-        const variables: LocationsQueryQueryVariables = {
-            page: pageNumber,
-            name: name
-        }
-        const data = await gqlRequest<Locations>(rickAndMortyEndpoint, LocationsQueryDocument, variables)
-
-        return Response.json({ data })
+        return Response.json({ locations })
     } catch (error) {
         return Response.json({ error: error }, { status: 500 })
     }
 
+}
+
+
+function transformCharactersResponseToLocation(characters: Characters): Locations {
+    const locations: Locations = { info: characters.info, results: [] }
+
+    if (!characters.results) {
+        return locations;
+    }
+
+    //@ts-ignore
+    for (let _character of characters.results) {
+        if (!_character) {
+            continue
+        }
+
+        const characterInfo: Character = { id: _character.id, image: _character.image, name: _character.name, status: _character.status, episode: [] }
+        //@ts-ignore
+        const locationIndex: number = locations.results?.findIndex((_location) => _location?.id === _character.location?.id) as number
+
+        if (locationIndex !== -1) {
+            (locations.results as Location[])[locationIndex].residents.push(characterInfo);
+        } else {
+            locations.results?.push({ ..._character.location, residents: [characterInfo] })
+        }
+
+    }
+
+    return locations;
+}
+
+function transformEpisodesResponseToLocation(episodes: Episodes): Locations {
+    const locations: Locations = { info: episodes.info, results: [] }
+
+    if (episodes.results) {
+        return locations;
+    }
+
+    let _episode: Episode
+    //@ts-ignore
+    for (_episode of episodes.results) {
+        for (let _character of _episode.characters) {
+            if (!_character) {
+                continue
+            }
+
+            const characterInfo: Character = { id: _character.id, image: _character.image, name: _character.name, status: _character.status, episode: [] }
+            //@ts-ignore
+            const locationIndex: number = locations.results?.findIndex((_location) => _location?.id === _character.location?.id) as number
+
+            if (locationIndex !== -1) {
+                (locations.results as Location[])[locationIndex].residents.push(characterInfo);
+            } else {
+                locations.results?.push({ ..._character.location, residents: [characterInfo] })
+            }
+
+        }
+    }
+
+    return locations;
 }
