00ecdb80ec78cc0f47a384f1c63e4826cec9b024
George Ndirangu <ndirangu.mepawa@gmail.com>
2024-02-19 16:03:33 +0300

    feat: implement infinite scroll

--- src/app/(frontend)/_components/Locations.tsx
+++ src/app/(frontend)/_components/Locations.tsx
@@ -3,41 +3,62 @@
 import {
   LocationsQueryQuery,
   Location as gqlLocation,
-  Locations as gqlLocations,
 } from "@/app/_graphql/types/graphql";
 import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import LocationCard from "./LocationCard";
 
 //add pagination and loading state
 export default function Locations() {
-  const [locations, setLocations] = useState<gqlLocations>({
-    __typename: undefined,
-    info: {},
-    results: [],
-  });
+  const [locations, setLocations] = useState<gqlLocation[]>([]);
   const [locationName, setLocationName] = useState("");
   const [characterName, setCharacterName] = useState("");
   const [episodeName, setEpisodeName] = useState("");
+  const [page, setPage] = useState(1);
+  const { ref, inView } = useInView();
 
   useEffect(() => {
-    fetchLocations(locationName, characterName, episodeName).then(
+    fetchLocations(locationName, characterName, episodeName, page).then(
       (_locations) => {
-        setLocations(_locations.locations as gqlLocations);
+        setLocations(_locations.locations?.results as gqlLocation[]);
         console.log("locations fetched", _locations.locations);
       }
     );
   }, []);
 
+  useEffect(() => {
+    if (inView) {
+      fetchMore();
+    }
+  }, [inView]);
+
+  const fetchMore = () => {
+    console.log("fetch more");
+    setPage(page + 1);
+
+    fetchLocations(locationName, characterName, episodeName, page).then(
+      (_locations) => {
+        const newList = [
+          ...locations,
+          ...(_locations.locations?.results as gqlLocation[]),
+        ];
+        setLocations(newList);
+      }
+    );
+  };
+
   return (
     <>
       <div className=" bg-gray-500 bg-opacity-20 px-10 py-10 mt-10 mb-20  h-[90%] rounded-2xl w-full">
         <div className="locations-list  h-[100%] overflow-auto">
-          {locations.results?.slice(0).map((_location) => (
+          {locations.slice(0).map((_location) => (
             <LocationCard
               key={_location?.id}
               location={_location as gqlLocation}
             ></LocationCard>
           ))}
+
+          <div ref={ref}>Loading...</div>
         </div>
       </div>
     </>
@@ -47,7 +68,8 @@ export default function Locations() {
 async function fetchLocations(
   locationName: string,
   characterName: string,
-  episodeName: string
+  episodeName: string,
+  page: number
 ): Promise<LocationsQueryQuery> {
   const res = await fetch(
     "/api/location?" +
@@ -55,6 +77,7 @@ async function fetchLocations(
         name: locationName,
         character: characterName,
         episode: episodeName,
+        page: page.toString(),
       })
   );
   if (!res.ok) {
